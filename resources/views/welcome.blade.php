42c3e4284464eb04a73ce36d7a546a44e029be23
VI-swolf <VI-swolf@users.noreply.github.com>
2019-10-31 14:33:28 +0100

    Änderung des Titels der Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello i Bims 1 Laravel</title>
+        <title>Willkommen beim Dualen-WG Manager</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">

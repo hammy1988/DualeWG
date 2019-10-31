2773d1c839694e0fa2b9796883eb2f379b91043c
hammy1988 <sebastian_pleier@web.de>
2019-10-31 14:45:44 +0100

    Revert "Update welcome.blade.php"
    This reverts commit 5abde0e9064b01783f94b3fb746f8951041c4891.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hallo Creative Informatics Team :)
+                    Laravel
                 </div>
 
                 <div class="links">

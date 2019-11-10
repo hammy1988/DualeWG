308cf9e47c1bcb146f26c1f576dd859181512858
hammy1988 <sebastian_pleier@web.de>
2019-11-10 22:25:04 +0100

    Update home.blade.php
    Test um User-Daten auszulesen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -15,6 +14,18 @@
                     @endif
 
                     You are logged in!
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">Benutzer Infos</div>
+                <div class="card-body">
+                    Benutzername: {{Auth::user()->name}}<br />
+                    Datenbank-ID: {{Auth::id()}}<br />
+                    <br />
+                    Login-Infos:<br />
+                    <code>
+                        {{Auth::user()}}
+                    </code>
                 </div>
             </div>
         </div>

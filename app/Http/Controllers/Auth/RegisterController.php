b026594be00ec59281bf839929fc34d5ccb0c09a
hammy1988 <sebastian_pleier@web.de>
2019-11-11 02:05:31 +0100

    Revert "Update des RegisterController"
    This reverts commit 81584eae9a924f506bfd2f10321707ab3eb14107.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use App\Role;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -62,15 +61,12 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data) {
-        $user = User::create([
+    protected function create(array $data)
+    {
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => (Role::where('name', 'new')->first()->id),
-
         ]);
-
-        return $user;
     }
 }

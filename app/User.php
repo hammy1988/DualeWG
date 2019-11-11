1ee0c2fe7c32a1198ddce19ca4c4cc9f7f930b67
hammy1988 <sebastian_pleier@web.de>
2019-11-11 02:06:51 +0100

    Kaputt

--- app/User.php
+++ app/User.php
@@ -38,6 +38,7 @@ class User extends Authenticatable
     ];
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     public function roles() {
         return $this->belongsTo(Role::class);
@@ -69,6 +70,8 @@ class User extends Authenticatable
     public function hasWGRole($roles) {
         return null !== $this->roles()->where('name', $roles)->first();
     }*/
+=======
+>>>>>>> parent of 99638b4... Zuweisungen von WG und Role erstellt
 =======
 >>>>>>> parent of 99638b4... Zuweisungen von WG und Role erstellt
 =======

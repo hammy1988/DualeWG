d3cef020da28c2c694326ea38b4dc4efbf59ddbd
hammy1988 <sebastian_pleier@web.de>
2019-11-11 16:08:27 +0100

    Middleware NoFlatshare

--- app/Http/Controllers/FlatshareChoiceController.php
+++ app/Http/Controllers/FlatshareChoiceController.php
@@ -14,6 +14,7 @@ class FlatshareChoiceController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('checkNoFlatshare');
     }
 
     /**

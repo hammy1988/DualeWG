0800d863e2bc5bc06d12f13683d1658320b4de88
Andreas <andreas.hofmann@hof-university.de>
2019-11-15 08:44:56 +0100

    Alles übersetzt
    Sprachen- Ordner mit
    composer require caouecs/laravel-lang:~4.0
    erstellt.
    de - Ordner in lang kopiert

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
-    | Password Reset Language Lines
+    | Password Reminder Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are the default lines which match reasons
@@ -13,10 +12,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password'  => 'Passwörter müssen mindestens 8 Zeichen lang sein und korrekt bestätigt werden.',
+    'reset'     => 'Das Passwort wurde zurückgesetzt!',
+    'sent'      => 'Passworterinnerung wurde gesendet!',
     'throttled' => 'Please wait before retrying.',
-
+    'token'     => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig oder abgelaufen.',
+    'user'      => 'Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.',
 ];

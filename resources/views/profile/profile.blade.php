46d223c62945932a031e62c2b390c55f3d8335d5
VI-llauterbach <57222854+VI-llauterbach@users.noreply.github.com>
2019-11-21 18:33:16 +0100

    Design Profil Seite

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,21 +1,41 @@
 @extends("layouts.app", ["title" => "Profil"])
 
+@section('headcss')
+    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+@endsection
+
 @section("content")
-    <h1>Profil: </h1>
-    <div id="dashContent">
-        Username: <br>
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Ihre Profil Informationen') }}</div>
 
-        E-Mail:
-    </div>
+                    <div class="card-body">
+                        Username: <br>
+                        E-Mail-Adresse: <br>
+                        BierBierBier!
+                    </div>
+                </div>
 
-    <div class="logininfo"> Sie sind angemeldet als: <a href=""> NAME </a>  | <a href=" {{route('logout')}}"       onclick="event.preventDefault();
+                <div class="logininfo"> Sie sind angemeldet als: <a href=""> NAME </a>  | <a href=" {{route('logout')}}"       onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">{{__('(Logout)')}}  </a>
 
 
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
 @endsection
-</div>
+

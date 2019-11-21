5dae5504d26413fd04c33cf3085ebb23bfff31a1
VI-llauterbach <57222854+VI-llauterbach@users.noreply.github.com>
2019-11-21 17:51:14 +0100

    Logout auch über Link im Fooder auf Profilverwaltungsseite möglich

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,5 +3,19 @@
 @section("content")
     <h1>Profil: </h1>
     <div id="dashContent">
+        Username: <br>
+
+
+        E-Mail:
     </div>
+
+    <div class="logininfo"> Sie sind angemeldet als: <a href=""> NAME </a>  | <a href=" {{route('logout')}}"       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">{{__('(Logout)')}}  </a>
+
+
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
 @endsection
+</div>

8233eb717fea0880ea68b9781db0365ce412c188
hammy1988 <sebastian_pleier@web.de>
2019-11-29 10:13:58 +0100

    IntelliSense für Laravel in PHPStorm

---
+++ _ide_helper_models.php
@@ -0,0 +1,95 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Flatshare
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $tagid
+ * @property int $admin_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\User $admin
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereAdminId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereTagid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Flatshare whereUpdatedAt($value)
+ */
+	class Flatshare extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Task
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Task whereUpdatedAt($value)
+ */
+	class Task extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $givenname
+ * @property string $name
+ * @property string $username
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $api_token
+ * @property int|null $flatshare_id
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Flatshare|null $flatshare
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereFlatshareId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereGivenname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
+ */
+	class User extends \Eloquent {}
+}
+

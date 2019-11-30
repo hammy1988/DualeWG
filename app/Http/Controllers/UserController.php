7a14a6f20de626b207b05efca1669da7813ae53e
hammy1988 <sebastian_pleier@web.de>
2019-11-30 18:27:13 +0100

    Fehlerbehandlung Backend für UserController (Profile)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Request;
 use App\Flatshare;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+
+
     /**
      * Display a listing of the resource.
      *
@@ -61,17 +64,11 @@ class UserController extends Controller
             $actUser = Auth::user();
             if (!(($actUser->isFlatshareAdmin() && $user->flatshare_id == $actUser->flatshare->id) ||
                 $user->id == Auth::id())) {
-
                 abort(403, 'Access denied');
+            }
 
-            } else {
-
-                if ($request->action == 'deleteFlatshare') {
-                    $user->flatshare_id = null;
-                    $user->flatsharejoin_at = null;
-                }
-                $user->save();
-
+            if ($request->action == 'deleteFlatshare') {
+                return $this->updateDeleteFlatshare($request, $id, $user);
             }
 
         } else if ($request->action == 'acceptFlatshare' ||
@@ -81,18 +78,16 @@ class UserController extends Controller
             $actUser = Auth::user();
             if (!($actUser->isFlatshareAdmin() &&
                   $user->flatshare_id == $actUser->flatshare->id)) {
-
                 abort(403, 'Access denied');
+            }
 
-            } else {
-
-                if ($request->action == 'acceptFlatshare') {
-                    $user->flatsharejoin_at = new \DateTime("now", new \DateTimeZone("UTC"));
-                }
-                if ($request->action == 'deniedFlatshare') {
-                    $user->flatshare_id = null;
-                    $user->flatsharejoin_at = null;
-                }
+            if ($request->action == 'acceptFlatshare') {
+                $user->flatsharejoin_at = new \DateTime("now", new \DateTimeZone("UTC"));
+                $user->save();
+            }
+            if ($request->action == 'deniedFlatshare') {
+                $user->flatshare_id = null;
+                $user->flatsharejoin_at = null;
                 $user->save();
             }
 
@@ -106,32 +101,66 @@ class UserController extends Controller
             }
 
             if ($request->action == 'updateFlatshare') {
-
-                $flatshare = Flatshare::findOrFail($request->flatshareid);
-                $user->flatshare_id = $flatshare->id;
-                $user->save();
-
-                if ($flatshare->admin_id == 0) {
-                    $flatshare->admin_id = $user->id;
-                    $flatshare->save();
-                }
-
-                return response()->json($user,200);
+                return $this->updateFlatshare($request, $id, $user);
             }
-
             if ($request->action == 'updateProfile') {
-
-                $user->givenname = $request->givenname;
-                $user->name = $request->name;
-                $user->email = $request->email;
-                $user->save();
-
-                return response()->json($user,200);
+                return $this->updateProfile($request, $id, $user);
             }
 
         }
 
         return $user;
+
+    }
+
+
+    private function updateDeleteFlatshare(Request $request, $id, $user)
+    {
+
+        $user->flatshare_id = null;
+        $user->flatsharejoin_at = null;
+        $user->save();
+
+        return response()->json($user,200);
+
+    }
+
+
+    private function updateFlatshare(Request $request, $id, $user)
+    {
+
+        $flatshare = Flatshare::findOrFail($request->flatshareid);
+        $user->flatshare_id = $flatshare->id;
+        $user->save();
+
+        if ($flatshare->admin_id == 0) {
+            $flatshare->admin_id = $user->id;
+            $flatshare->save();
+        }
+
+        return response()->json($user,200);
+
+    }
+
+    private function updateProfile(Request $request, $id, $user)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'givenname' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', ('unique:users,email,' . $user->id)],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 422);
+        }
+
+        $user->givenname = $request->givenname;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json($user,200);
 
     }
 
